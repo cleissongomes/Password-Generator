cce75950aafc29aad683e959a6ce4d443f14a7d0
cleissongomes <cleissongomes777@gmail.com>
2023-06-02 22:00:14 -0300

    first commit

---
+++ src/modules/formGeneratesPassword.js
@@ -0,0 +1,8 @@
+const generatedPassword = document.querySelector('.generated-password');
+const characters = document.querySelector('.characters');
+const chkNumbers = document.querySelector('.chk-numbers');
+const chkCapitalLetters = document.querySelector('.chk-capital-letters');
+const chkLowerCase = document.querySelector('.chk-lower-case');
+const chkSymbols = document.querySelector('.chk-symbols');
+const generatePassword = document.querySelector('.generate-password');
+
