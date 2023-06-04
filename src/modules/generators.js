13fffe84a926a6afce2edb0cad6e436f9dbcfa87
cleissongomes <cleissongomes777@gmail.com>
2023-06-04 12:32:40 -0300

    feat: Add generatePassword function in generators.js

--- src/modules/generators.js
+++ src/modules/generators.js
@@ -1,9 +1,22 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const generateCapitalLetter = () => String.fromCharCode(rand(65, 91));
-const generateLowercaseLetter = () => String.fromCharCode(rand(97, 123));
+const generateLowerCaseLetter = () => String.fromCharCode(rand(97, 123));
 const generateNumber = () => String.fromCharCode(rand(48, 58));
 const symbols = ',.;~^[]{}!@#$*()_+=-';
-const generateSymbol = () => symbols[rand(0, symbols.length)];
+const generateSymbols = () => symbols[rand(0, symbols.length)];
+
+function generatePassword(quantity, capitalLetters, lowerCaseLetters, numbers, symbols) {
+    const arrayPassword = [];
+    quantity = Number(quantity);
+
+    for(let i = 0; i < quantity; i++) {
+        capitalLetters && arrayPassword.push(generateCapitalLetter());
+        lowerCaseLetters && arrayPassword.push(generateLowerCaseLetter());
+        numbers && arrayPassword.push(generateNumber());
+        symbols && arrayPassword.push(generateSymbols());
+    }
+    return arrayPassword.join('').slice(0, quantity);
+}
 
 
 
