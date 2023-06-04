fdf8b0258aa33d708b37a2081d3e623900eb3ffd
cleissongomes <cleissongomes777@gmail.com>
2023-06-04 12:16:04 -0300

    feat: Add generateCapitalLetter, generateLowercaseLetter, generateNumber, symbols, and generateSymbol variables in generators.js

--- src/modules/generators.js
+++ src/modules/generators.js
@@ -1,1 +1,9 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const generateCapitalLetter = () => String.fromCharCode(rand(65, 91));
+const generateLowercaseLetter = () => String.fromCharCode(rand(97, 123));
+const generateNumber = () => String.fromCharCode(rand(48, 58));
+const symbols = ',.;~^[]{}!@#$*()_+=-';
+const generateSymbol = () => symbols[rand(0, symbols.length)];
+
+
+
